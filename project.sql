9ab1da3f79963b1ed4c4a6ae690a6ff0e1f90e05
Alexis M. Webster <webstea1@arch08.cis.gvsu.edu>
2022-03-30 19:39:49 -0400

    Formatting changes

--- project.sql
+++ project.sql
@@ -12,24 +12,24 @@ In the DDL, every IC must have a unique name; e.g. IC5, IC10, IC15, etc.
 --
 SET FEEDBACK OFF
 < The INSERT statements that populate the tables>
-Important: Keep the number of rows in each table small enough so that the results of your
-queries can be verified by hand. See the Sailors database as an example.
+--Important: Keep the number of rows in each table small enough so that the results of your
+--queries can be verified by hand. See the Sailors database as an example.
 SET FEEDBACK ON
 COMMIT;
 --
 < One query (per table) of the form: SELECT * FROM table; in order to display your database >
 --
 < The SQL queries>. Include the following for each query:
-	− A comment line stating the query number and the feature(s) it demonstrates
-(e.g. -- Q25 – correlated subquery).
-	− A comment line stating the query in English.
-− The SQL code for the query.
+--	− A comment line stating the query number and the feature(s) it demonstrates
+--(e.g. -- Q25 – correlated subquery).
+--	− A comment line stating the query in English.
+--	− The SQL code for the query.
 --
 < The insert/delete/update statements to test the enforcement of ICs >
-Include the following items for every IC that you test (Important: see the next section titled
-“Submit a final report” regarding which ICs you need to test).
-− A comment line stating: Testing: < IC name>
-− A SQL INSERT, DELETE, or UPDATE that will test the IC.
+/*Include the following items for every IC that you test (Important: see the next section titled
+“Submit a final report” regarding which ICs you need to test).*/
+	--− A comment line stating: Testing: < IC name>
+	--− A SQL INSERT, DELETE, or UPDATE that will test the IC.
 COMMIT;
 --
 SPOOL OFF
